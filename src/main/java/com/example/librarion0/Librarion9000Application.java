27660635d884086746d9e62f26da7d0721e0f6e5
vbalazs23 <75390188+vbalazs23@users.noreply.github.com>
2020-12-03 17:37:47 +0100

    Add files via upload

---
+++ src/main/java/com/example/librarion0/Librarion9000Application.java
@@ -0,0 +1,13 @@
+package com.example.librarion0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Librarion9000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Librarion9000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/librarion0/Librarion9000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarion0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Librarion9000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
