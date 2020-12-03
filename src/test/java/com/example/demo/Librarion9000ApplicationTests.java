940e3b3fe02809ddc88b57c5b780bd7f4f054358
vbalazs23 <75390188+vbalazs23@users.noreply.github.com>
2020-12-03 17:27:38 +0100

    Add files via upload

---
+++ src/test/java/com/example/demo/Librarion9000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Librarion9000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
